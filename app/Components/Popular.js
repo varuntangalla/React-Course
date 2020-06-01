ea9164dc130a744cdb63ef92804b3ed55e9dc527
stangalla <stangalla@miraclesoft.com>
2020-06-01 17:59:05 -0400

    Implementing code as React Fragment without state

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -1,21 +1,8 @@
 import React from 'react'
 
-export default class Popular extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      selectedLanguage: 'All'
-    }
+function LanguagesNav ({ selected, onUpdateLanguage }) {
 
-    this.updateLanguage = this.updateLanguage.bind(this)
-  }
-  updateLanguage (selectedLanguage) {
-    this.setState({
-      selectedLanguage
-    })
-  }
-  render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
     return (
@@ -24,13 +11,44 @@ export default class Popular extends React.Component {
           <li key={language}>
             <button
               className='btn-clear nav-link'
-              style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
-              onClick={() => this.updateLanguage(language)}>
+              style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+              onClick={() => onUpdateLanguage(language)}>
               {language}
             </button>
           </li>
         ))}
       </ul>
+    )
+
+}
+
+export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+   
+  
+   this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+  render() {
+
+    const { selectedLanguage } = this.state
+
+    return(
+        <React.Fragment>
+            <LanguagesNav 
+            selected={selectedLanguage} 
+            onUpdateLanguage={this.updateLanguage}            
+            />
+        </React.Fragment>
     )
   }
 }
