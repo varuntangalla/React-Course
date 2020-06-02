ac96f591572999930509fa2db03b480dbc8ee0e3
stangalla <stangalla@miraclesoft.com>
2020-06-02 17:17:47 -0400

    Rendering Player Input

--- app/Components/Battle.js
+++ app/Components/Battle.js
@@ -101,11 +101,48 @@ PlayerInput.propTypes = {
 }
 
 export default class Battle extends React.Component {
+
+  contructor(props) {
+    super(props)
+
+    this.state = {
+
+      playerOne: null,
+      playerTwo: null,
+    }
+    this.handleSubmit = this.handleSubmit(bind);
+  }
+  handleSubmit(id, player) {
+      this.setState({
+        [id]: player
+      })
+  }
     render(){
+      const { playerOne, playerTwo } = this.state
+
         return (
         <React.Fragment>
             <Instructions />
-                  </React.Fragment>
+
+         <div className='players-container'>
+           <h1 className='center-text header-lg'>Players</h1>
+           <div className='row space-around'>
+             {playerOne === null && (
+               <PlayerInput 
+                  label='Player One'
+                  onSubmit={(player) => this.handleSubmit('playerOne', player)}
+                />  
+             )}
+
+            {playerTwo === null && (
+               <PlayerInput 
+                  label='Player Two'
+                  onSubmit={ (player) => this.handleSubmit('playerTwo', player) }
+                />  
+             )}
+           </div>
+           </div>   
+        </React.Fragment>
         )      
     }
 }
