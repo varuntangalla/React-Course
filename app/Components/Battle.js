c1596e399449a38f8049162f678077d09580270f
stangalla <stangalla@miraclesoft.com>
2020-06-02 17:30:52 -0400

    Render PlayerInput

--- app/Components/Battle.js
+++ app/Components/Battle.js
@@ -1,76 +1,60 @@
-import React from 'react' 
-import { FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa'
+import React from 'react'
+import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 
-
-
 function Instructions () {
-    return (
-      <div className='instructions-container'>
-          <h1 className='center-text header-lg'>
-              Instructions
-          </h1>
-          <ol className='container-sm grid center-text battle-instructions'>
-              <li>
-                <h3 className='header-sm'>
-                 Enter two Github users    
-                </h3>  
-                <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140}/>
-              </li>
-              <li>
-                <h3 className='header-sm'>
-                 Battle    
-                </h3>  
-                <FaFighterJet className='bg-light' color='#727272' size={140}/>
-              </li>
-              <li>
-                <h3 className='header-sm'>
-                 See the winners    
-                </h3>  
-                <FaTrophy className='bg-light' color='rgb(255, 210, 0)' size={140}/>
-              </li>  
-          </ol>
-      </div>  
-    )
+  return (
+    <div className='instructions-container'>
+      <h1 className='center-text header-lg'>
+        Instructions
+      </h1>
+      <ol className='container-sm grid center-text battle-instructions'>
+        <li>
+          <h3 className='header-sm'>Enter two Github users</h3>
+          <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
+        </li>
+        <li>
+          <h3 className='header-sm'>Battle</h3>
+          <FaFighterJet className='bg-light' color='#727272' size={140} />
+        </li>
+        <li>
+          <h3 className='header-sm'>See the winners</h3>
+          <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
+        </li>
+      </ol>
+    </div>
+  )
 }
-
 
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-
       username: ''
     }
+
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
   handleSubmit(event) {
+    event.preventDefault()
 
-    event.preventDefault() 
     this.props.onSubmit(this.state.username)
-
   }
-
   handleChange(event) {
     this.setState({
-
       username: event.target.value
-
-
-
     })
   }
   render() {
-
-    return(
+    return (
       <form className='column player' onSubmit={this.handleSubmit}>
         <label htmlFor='username' className='player-label'>
           {this.props.label}
         </label>
         <div className='row player-inputs'>
-          <input 
+          <input
             type='text'
             id='username'
             className='input-light'
@@ -79,13 +63,12 @@ class PlayerInput extends React.Component {
             value={this.state.username}
             onChange={this.handleChange}
           />
-
-          <button 
-          className='btn dark-btn' 
-          type='submit'
-          disabled={!this.state.username}
+          <button
+            className='btn dark-btn'
+            type='submit'
+            disabled={!this.state.username}
           >
-          Submit
+            Submit
           </button>
         </div>
       </form>
@@ -93,56 +76,53 @@ class PlayerInput extends React.Component {
   }
 }
 
-
 PlayerInput.propTypes = {
-
   onSubmit: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired
 }
 
 export default class Battle extends React.Component {
-
-  contructor(props) {
+  constructor(props) {
     super(props)
 
     this.state = {
-
       playerOne: null,
       playerTwo: null,
     }
-    this.handleSubmit = this.handleSubmit(bind);
+
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
   handleSubmit(id, player) {
-      this.setState({
-        [id]: player
-      })
+    this.setState({
+      [id]: player
+    })
   }
-    render(){
-      const { playerOne, playerTwo } = this.state
+  render() {
+    const { playerOne, playerTwo } = this.state
 
-        return (
-        <React.Fragment>
-            <Instructions />
+    return (
+      <React.Fragment>
+        <Instructions />
 
-         <div className='players-container'>
-           <h1 className='center-text header-lg'>Players</h1>
-           <div className='row space-around'>
-             {playerOne === null && (
-               <PlayerInput 
-                  label='Player One'
-                  onSubmit={(player) => this.handleSubmit('playerOne', player)}
-                />  
-             )}
+        <div className='players-container'>
+          <h1 className='center-text header-lg'>Players</h1>
+          <div className='row space-around'>
+            {playerOne === null && (
+              <PlayerInput
+                label='Player One'
+                onSubmit={(player) => this.handleSubmit('playerOne', player)}
+              />
+            )}
 
             {playerTwo === null && (
-               <PlayerInput 
-                  label='Player Two'
-                  onSubmit={ (player) => this.handleSubmit('playerTwo', player) }
-                />  
-             )}
-           </div>
-           </div>   
-        </React.Fragment>
-        )      
-    }
+              <PlayerInput
+                label='Player Two'
+                onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+              />
+            )}
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
